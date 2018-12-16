c241b1b68e81bdde9ce74ff79dab3a96b1d5e917
denis96z <zinoviev.denis@yandex.ru>
2018-12-16 22:21:27 +0300

    main fixed

--- main.go
+++ main.go
@@ -1,47 +1,40 @@
 package main
 
 import (
-	"github.com/bombergame/multiplayer-service/game/fields"
-	"github.com/bombergame/multiplayer-service/game/objects/players"
-	"github.com/bombergame/multiplayer-service/game/physics"
+	"github.com/bombergame/common/logs"
+	restful "github.com/bombergame/common/rest"
+	"github.com/bombergame/multiplayer-service/services/rest"
+	"github.com/bombergame/multiplayer-service/utils"
+	"os"
+	"os/signal"
 )
 
 func main() {
-	f := fields.NewField(physics.GetSize2D(100, 100))
+	logger := logs.NewLogger()
 
-	p := make(map[int64]*players.Player, 0)
-	var i int64
-	for i = 0; i < 10; i++ {
-		p[i] = players.NewPlayer(i)
+	restSrv := rest.NewService(
+		rest.Config{},
+		rest.Components{
+			RoomsManager: utils.NewRoomsManager(),
+			Components: restful.Components{
+				Logger:      logger,
+				AuthManager: utils.NewAuthManager(),
+			},
+		},
+	)
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Interrupt)
+
+	go func() {
+		if err := restSrv.Run(); err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	<-ch
+
+	if err := restSrv.Shutdown(); err != nil {
+		logger.Fatal(err)
 	}
-
-	f.PlaceObjects(p)
-
-	//logger := logs.NewLogger()
-	//
-	//restSrv := rest.NewService(
-	//	rest.Config{},
-	//	rest.Components{
-	//		RoomsManager: utils.NewRoomsManager(),
-	//		Components: restful.Components{
-	//			Logger:      logger,
-	//			AuthManager: utils.NewAuthManager(),
-	//		},
-	//	},
-	//)
-	//
-	//ch := make(chan os.Signal)
-	//signal.Notify(ch, os.Interrupt)
-	//
-	//go func() {
-	//	if err := restSrv.Run(); err != nil {
-	//		logger.Fatal(err)
-	//	}
-	//}()
-	//
-	//<-ch
-	//
-	//if err := restSrv.Shutdown(); err != nil {
-	//	logger.Fatal(err)
-	//}
 }
