7d1ed26fba24a0b317e62f010c5f45a55537f12b
denis96z <zinoviev.denis@yandex.ru>
2018-12-16 13:45:30 +0300

    refactoring

--- game/objects/players/commands/command.go
+++ game/objects/players/commands/command.go
@@ -3,12 +3,13 @@ package playercommands
 type Cmd string
 
 const (
-	Prefix    = "player."
-	Stop      = Prefix + "stop"
-	MoveUp    = Prefix + "move.up"
-	MoveDown  = Prefix + "move.down"
-	MoveLeft  = Prefix + "move.left"
-	MoveRight = Prefix + "move.right"
+	Prefix = "player."
+
+	MovementPrefix = "move."
+	MoveUp         = MovementPrefix + "up"
+	MoveDown       = MovementPrefix + "down"
+	MoveLeft       = MovementPrefix + "left"
+	MoveRight      = MovementPrefix + "right"
 )
 
 type CmdChan chan Cmd
