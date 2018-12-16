b798b1dd5164a77db28b9cf76793b11da73fdce4
denis96z <zinoviev.denis@yandex.ru>
2018-12-16 17:16:59 +0300

    some messages added

--- game/rooms/messages/messageData.go
+++ game/rooms/messages/messageData.go
@@ -3,21 +3,31 @@ package roommessages
 //go:generate easyjson
 
 import (
-	"github.com/bombergame/multiplayer-service/game/rooms/state"
+	"github.com/bombergame/multiplayer-service/game/rooms/events"
 	"github.com/satori/go.uuid"
 )
 
 const (
-	MessageType    = "room"
-	NewPlayerEvent = "newplayer"
+	MessageType = "room"
 )
 
 //easyjson:json
 type MessageData struct {
-	ID        uuid.UUID       `json:"id"`
-	GameState gamestate.State `json:"game_state"`
+	ID uuid.UUID `json:"id"`
+}
+
+//easyjson:json
+type InfoMessageData struct {
+	MessageData
+}
+
+//easyjson:json
+type EventMessageData struct {
+	MessageData
+	Event roomevents.Event `json:"event"`
 }
 
 //easyjson:json
 type NewPlayerMessageData struct {
+	ProfileID int64 `json:"id"`
 }
